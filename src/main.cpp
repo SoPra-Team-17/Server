18abf0f307b98ab577cc67fb9f979e0d8c08cf85
Dominik <dominik.authaler@uni-ulm.de>
2020-04-01 22:01:22 +0200

    cli11 without submodule

---
+++ src/main.cpp
@@ -0,0 +1,50 @@
+/**
+ * @file   main.cpp
+ * @author Dominik Authaler
+ * @date   01.04.2020 (creation)
+ * @brief
+ */
+
+#include "CLI/CLI.hpp"
+
+constexpr int maxVerbosity     =  5;
+constexpr int defaultVerbosity =  1;
+constexpr int defaultPort      = 17;
+
+int main(int argc, char *argv[]) {
+    CLI::App app;
+
+    std::string  characterPath;
+    std::string  matchPath;
+    std::string  scenarioPath;
+
+    unsigned int verbosity = defaultVerbosity;
+    unsigned int port      = defaultPort;
+
+    std::vector<std::string> additionalOptions;
+
+    app.add_option("--config-charset,-c", characterPath,
+            "Path to the character configuration file")->required()->check(CLI::ExistingFile);
+    app.add_option("--config-match,-m", matchPath,
+            "Path to the match configuration file")->required()->check(CLI::ExistingFile);
+    app.add_option("--config-scenario,-s", scenarioPath,
+            "Path to the scenario configuration file")->required()->check(CLI::ExistingFile);
+    app.add_option("--verbosity,-v", verbosity,
+            "Logging verbosity")->check(CLI::Range(0, maxVerbosity));
+    app.add_option("--port,-p", port, "Port used by the server");
+    app.add_option("--x", additionalOptions, "Additional key value pairs");
+
+    CLI11_PARSE(app, argc, argv);
+
+    std::cout << "Character configuration: " << characterPath << std::endl;
+    std::cout << "Match configuration:     " << matchPath     << std::endl;
+    std::cout << "Scenario configuration:  " << scenarioPath  << std::endl;
+    std::cout << "Verbosity:               " << verbosity     << std::endl;
+    std::cout << "Port:                    " << port          << std::endl;
+    std::cout << "Additional options: " << std::endl;
+    for (unsigned int i = 0; i < additionalOptions.size(); i+= 2) {
+        std::cout << "Key: " << additionalOptions[i] << "\tValue: " << additionalOptions[i+1] << std::endl;
+    }
+
+    return 0;
+}
