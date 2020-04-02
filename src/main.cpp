1eb6e6cb1fee4fc57269398400e4741790729b81
Dominik <dominik.authaler@uni-ulm.de>
2020-04-02 10:03:14 +0200

    ottojo's comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,36 @@
 /**
  * @file   main.cpp
- * @author Dominik Authaler
+ * @author Dominik Authaler, Jonas Otto
  * @date   01.04.2020 (creation)
  */
 
 #include "CLI/CLI.hpp"
 #include "spdlog/spdlog.h"
-constexpr unsigned int maxVerbosity     =  5;
-constexpr unsigned int defaultVerbosity =  1;
-constexpr unsigned int defaultPort      = 17;
+
+constexpr unsigned int maxVerbosity = 5;
+constexpr unsigned int defaultVerbosity = 1;
+constexpr unsigned int defaultPort = 17;
 
 int main(int argc, char *argv[]) {
     CLI::App app;
 
-    std::string  characterPath;
-    std::string  matchPath;
-    std::string  scenarioPath;
+    std::string characterPath;
+    std::string matchPath;
+    std::string scenarioPath;
 
     unsigned int verbosity = defaultVerbosity;
-    unsigned int port      = defaultPort;
+    unsigned int port = defaultPort;
 
     std::vector<std::string> additionalOptions;
 
     app.add_option("--config-charset,-c", characterPath,
-            "Path to the character configuration file")->required()->check(CLI::ExistingFile);
+                   "Path to the character configuration file")->required()->check(CLI::ExistingFile);
     app.add_option("--config-match,-m", matchPath,
-            "Path to the match configuration file")->required()->check(CLI::ExistingFile);
+                   "Path to the match configuration file")->required()->check(CLI::ExistingFile);
     app.add_option("--config-scenario,-s", scenarioPath,
-            "Path to the scenario configuration file")->required()->check(CLI::ExistingFile);
+                   "Path to the scenario configuration file")->required()->check(CLI::ExistingFile);
     app.add_option("--verbosity,-v", verbosity,
-            "Logging verbosity")->check(CLI::Range(maxVerbosity));
+                   "Logging verbosity")->check(CLI::Range(maxVerbosity));
     app.add_option("--port,-p", port, "Port used by the server");
     app.add_option("--x", additionalOptions, "Additional key value pairs");
 
@@ -43,11 +44,11 @@ int main(int argc, char *argv[]) {
     spdlog::info(" -> character configuration: {}", characterPath);
     spdlog::info(" -> match configuration:     {}", matchPath);
     spdlog::info(" -> scenario configuration:  {}", scenarioPath);
-    spdlog::info(" -> verbosity: {}", verbosity);
-    spdlog::info(" -> port: {}", port);
+    spdlog::info(" -> verbosity:               {}", verbosity);
+    spdlog::info(" -> port:                    {}", port);
     spdlog::info(" -> additional:");
-    for (unsigned int i = 0; i < additionalOptions.size(); i+= 2) {
-        spdlog::info("\t {} = {}", additionalOptions[i], additionalOptions[i+1]);
+    for (unsigned int i = 0; i + 1 < additionalOptions.size(); i+= 2) {
+        spdlog::info("\t {} = {}", additionalOptions.at(i), additionalOptions.at(i+1));
     }
 
     return 0;
