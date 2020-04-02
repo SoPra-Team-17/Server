a4bee9bfccb60b8174c8b7fa94602bc49e0bdf90
Jonas Otto <jottosmail@gmail.com>
2020-04-02 14:36:14 +0200

    fail dependency install script if error occurs, build CLI11 without tests

--- installDependencies.sh
+++ installDependencies.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+# Setup
+# Exit the script if any command fails
+set -e
+
 # CLI11
 cd /tmp
 git clone https://github.com/CLIUtils/CLI11.git
 cd CLI11
 mkdir build && cd build
-cmake ..
+cmake -DCLI11_BUILD_TESTS=false ..
 make -j$(nproc)
 sudo make install
 
