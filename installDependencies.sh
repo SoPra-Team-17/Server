95f28f213cd8075ff5bc6a11fac9c8ab105bd151
Jonas Otto <jottosmail@gmail.com>
2020-05-02 20:27:31 +0200

    add afsm dependency

--- installDependencies.sh
+++ installDependencies.sh
@@ -10,6 +10,24 @@ set -e
 # Dependencies from WebsocketCPP
 ./external/WebsocketCPP/installDependencies.sh
 
+# afsm
+cd /tmp
+git clone --depth 1 https://github.com/zmij/afsm.git
+cd afsm
+mkdir build && cd build
+cmake -DAFSM_BUILD_TESTS=false ..
+make -j$(nproc)
+sudo make install
+
+# metapushkin (asfm dependency)
+cd /tmp
+git clone --depth 1 https://github.com/zmij/metapushkin.git
+cd metapushkin
+mkdir build && cd build
+cmake -DMETA_BUILD_TESTS=false ..
+make -j$(nproc)
+sudo make install
+
 # CLI11
 cd /tmp
 git clone https://github.com/CLIUtils/CLI11.git
