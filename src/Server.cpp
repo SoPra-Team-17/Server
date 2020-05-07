6f868ccddb078d273555e63063333be23b3f8982
Jonas Otto <jottosmail@gmail.com>
2020-05-07 12:28:51 +0200

    add all message listeners

--- src/Server.cpp
+++ src/Server.cpp
@@ -95,7 +95,16 @@ Server::Server(uint16_t port, unsigned int verbosity, const std::string &charact
         fsm.process_event(msg);
     });
 
-    // TODO register handlers for more messages
+    auto forwardMessage = [&fsm](auto msg) { fsm.process_event(msg); };
+
+    //router.addReconnectListener(forwardMessage);
+    //router.addItemChoiceListener(forwardMessage);
+    //router.addEquipmentChoiceListener(forwardMessage);
+    router.addGameOperationListener(forwardMessage);
+    router.addGameLeaveListener(forwardMessage);
+    //router.addPauseRequestListener(forwardMessage);
+    //router.addMetaInformationRequestListener(forwardMessage);
+    //router.addReplayRequestListener(forwardMessage);
 }
 
 void Server::configureLogging() const {
