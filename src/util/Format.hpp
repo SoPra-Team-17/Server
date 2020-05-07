e96ded3094c595d9cdf1d767d34d25436a27f858
Jonas Otto <jottosmail@gmail.com>
2020-05-07 23:48:50 +0200

    add missing file Format.hpp

---
+++ src/util/Format.hpp
@@ -0,0 +1,19 @@
+//
+// Created by jonas on 07.05.20.
+//
+
+#ifndef SERVER017_FORMAT_HPP
+#define SERVER017_FORMAT_HPP
+
+#include <string>
+#include <nlohmann/json.hpp>
+
+namespace fmt{
+    template<typename T>
+    std::string json(T t){
+        nlohmann::json j = t;
+        return j;
+    }
+}
+
+#endif //SERVER017_FORMAT_HPP
