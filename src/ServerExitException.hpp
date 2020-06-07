ece8b35f66469d7fb2082d25d9e942025783e74a
Jonas Otto (ottojo) <jottosmail@gmail.com>
2020-06-07 17:34:23 +0200

    fix config names in dockerfile

---
+++ src/ServerExitException.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file ServerExitException.hpp
+ * @author jonas
+ * @date 6/7/20
+ * Description here TODO
+ */
+
+#ifndef SERVER017_SERVEREXITEXCEPTION_HPP
+#define SERVER017_SERVEREXITEXCEPTION_HPP
+
+
+class ServerExitException {
+
+};
+
+
+#endif //SERVER017_SERVEREXITEXCEPTION_HPP
