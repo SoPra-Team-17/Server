bdfbf707ac73274c1fad36fc8b2dd430d853e4de
Jonas Otto (ottojo) <jottosmail@gmail.com>
2020-07-21 15:38:27 +0200

    crash -> gh issue

---
+++ execStopPost.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+GH_TOKEN=""
+GH:USER=""
+URL="https://api.github.com/repos/SoPra-Team-17/Server/issues"
+if [ $EXIT_STATUS -ne 0 ]; then
+    LOG=$(journalctl _SYSTEMD_INVOCATION_ID="$INVOCATION_ID")
+    MESSAGE="{\"title\":\"Crash: $(date)\", \"body\": \"<details>
+<summary>Log</summary>
+
+\`\`\`
+$LOG
+\`\`\`
+</details>\"}"
+
+    curl \
+        -X POST \
+        -u "$GH_USER:$GH_TOKEN" \
+        -H "Accept: application/vnd.github.v3+json" \
+        "$URL" \
+        -d "${MESSAGE//$'\n'/\\n}"
+fi
+
